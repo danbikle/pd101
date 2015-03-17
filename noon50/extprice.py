f2a401c1dc4c2d245b709760e3e56e6ace98d262
Dan Bikle <dan.bikle@gmail.com>
2015-03-17 21:37:58 +0000

    enhancements

--- noon50/extprice.py
+++ noon50/extprice.py
@@ -13,10 +13,16 @@ date_s = date_l[1]+date_l[2]
 mydt   = datetime.datetime.strptime(date_s, " %b %d %Y")
 mydt_s = mydt.strftime('%Y-%m-%d')
 
+# GSPC needs special handling here:
 span1      = soup.find(id="yfs_l10_^gspc")
+
 gspc_price = span1.string.replace(',','')
 gspc_s     = mydt_s+','+gspc_price+"\n"
 gspcf      = open('GSPCrecent.csv','w')
 gspcf.write(gspc_s)
 gspcf.close()
+
+# I should use a loop on the other tkrs
+for tkr in ['EEM','EFA']:
+  print tkr
 
