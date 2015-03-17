f44250597484655d6f4e4e05ad52f44322854cb5
Dan Bikle <dan.bikle@gmail.com>
2015-03-17 21:21:53 +0000

    enhancements

--- noon50/extprice.py
+++ noon50/extprice.py
@@ -2,11 +2,20 @@
 
 # This script should extract recent prices from html
 
-import pdb
 import bs4
-soup = bs4.BeautifulSoup(open("/tmp/noon50/GSPC.html"))
+import datetime
+import pdb
+
+soup   = bs4.BeautifulSoup(open("/tmp/noon50/GSPC.html"))
+span0  = soup.find(id="yfs_market_time")
+date_l = span0.string.split(",")[:3]
+date_s = date_l[1]+date_l[2]
+mydt   = datetime.datetime.strptime(date_s, " %b %d %Y")
+mydt_s = mydt.strftime('%Y-%m-%d')
 
 span1 = soup.find(id="yfs_l10_^gspc")
 pdb.set_trace()
 gspc_price = span1.string.replace(',','')
+
+
 
