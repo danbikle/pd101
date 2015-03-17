06c5425ca1f72a94e3dbd7fb9e93c9157ea0187e
Dan Bikle <dan.bikle@gmail.com>
2015-03-17 21:33:03 +0000

    enhancements

--- noon50/extprice.py
+++ noon50/extprice.py
@@ -1,6 +1,6 @@
 # /home/ann/pd101/noon50/extprice.py
 
-# This script should extract recent prices from html
+# This script should extract recent date, prices from html
 
 import bs4
 import datetime
@@ -13,13 +13,10 @@ date_s = date_l[1]+date_l[2]
 mydt   = datetime.datetime.strptime(date_s, " %b %d %Y")
 mydt_s = mydt.strftime('%Y-%m-%d')
 
-span1 = soup.find(id="yfs_l10_^gspc")
-pdb.set_trace()
+span1      = soup.find(id="yfs_l10_^gspc")
 gspc_price = span1.string.replace(',','')
-gspc_s = mydt_s+','+gspc_price+"\n"
-gspcf = open('recent_gspc.csv','w')
+gspc_s     = mydt_s+','+gspc_price+"\n"
+gspcf      = open('GSPCrecent.csv','w')
 gspcf.write(gspc_s)
 gspcf.close()
-
-
 
