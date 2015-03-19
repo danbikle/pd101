2ffe878c6c14cca2405e6dfeb05cc2c95ca0f76b
Dan Bikle <dan.bikle@gmail.com>
2015-03-19 10:32:03 +0000

    plot-enhancements

---
+++ plot/plotem.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# /home/ann/pd101/plot/plotem.bash
+
+# This script should plot data in a CSV file
+
+# Demo:
+# /home/ann/pd101/plot/plotem.bash /tmp/pd101/prdf1.csv
+
+if [ $# -ne 1 ]
+then
+  echo I need 1 arg.
+  echo Demo:
+  echo $0 /tmp/pd101/prdf1.csv
+  exit 1
+fi
+
+echo Working...
+
+python /home/ann/pd101/plotem.py $1
+
+exit
