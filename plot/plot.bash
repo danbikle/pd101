0108df221ce7c065e4a90a8296148fd43c63be27
Dan Bikle <dan.bikle@gmail.com>
2015-03-19 09:56:24 +0000

    plot-enhancements

--- plot/plot.bash
+++ plot/plot.bash
@@ -6,6 +6,10 @@
 
 mkdir -p /tmp/pd101/
 cd       /tmp/pd101/
-/home/ann/pd101/plot/wgetem.bash
+echo /home/ann/pd101/plot/wgetem.bash
 
 # Now I should have new CSV files in /tmp/pd101/
+
+set -x
+# I should create wide1.csv
+sqlite3 pd101.db '.read /home/ann/pd101/plot/joinem.sql'
